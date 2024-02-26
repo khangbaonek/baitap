acfbcb810135519548bd6e3339a4c235eae4abdc
ADMIN <ADMIN@LAB1-MAY19>
2024-02-26 08:32:37 +0700

    Add project files.

---
+++ baitap/baitap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tong;
+	cout << "CHUONG TRINH TINH TONG\n";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "Nhap so thu hai:";
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << "+" << so2 << "="
+		<< tong
+		<< endl;
+		return 0;
+}
